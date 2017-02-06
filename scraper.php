29fede3035c5057737dfba551f529e7c210cbd4a
vikash12345 <vikash@backgroundcheck.email>
2017-02-06 10:48:17 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,6 +1,6 @@
-<?php
-	include "simple_html_dom.php";
-	$MyWebsite = ("http://web.archive.org/web/20140824112027/http://www.peshawarhighcourt.gov.pk/PHCCMS/reportedJudgments.php?action=search");
- 	 $html  = file_get_html($MyWebsite);
-	echo $html;
-  ?>
+<?php 
+require 'simple_html_dom.php';
+ $MyWebsite = 'http://web.archive.org/web/20140824112027/http://www.peshawarhighcourt.gov.pk/PHCCMS/reportedJudgments.php?action=search';
+ $html = file_get_html($MyWebsite);
+
+?>
