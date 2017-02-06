b003af749b0954395f6e57b0fc5f9ce2cca38ff8
vikash12345 <vikash@backgroundcheck.email>
2017-02-06 10:50:09 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,5 +2,6 @@
 require 'simple_html_dom.php';
  $MyWebsite = 'http://www.peshawarhighcourt.gov.pk/PHCCMS/reportedJudgments.php?action=search';
  $html = file_get_html($MyWebsite);
+echo $html;
 
 ?>
