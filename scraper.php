2c2164361d771de641eef4a528d0d29a1b7d3b83
vikash12345 <vikash@backgroundcheck.email>
2017-02-06 11:30:39 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,9 +1,12 @@
 <?php 
-require 'simple_html_dom.php';
- $MyWebsite = 'http://www.peshawarhighcourt.gov.pk/PHCCMS/reportedJudgments.php?action=search';
- $html = file_get_html($MyWebsite);
-//This is for Remarks
-echo $html->find("//*[@id='employee_list']/tbody/tr[1]/td[2]", 0) . '<br/>';
-
-
+	require 'simple_html_dom.php';
+	$MyWebsite = 'http://www.peshawarhighcourt.gov.pk/PHCCMS/reportedJudgments.php?action=search';
+	$html = file_get_html($MyWebsite);
+	echo $html;
+	foreach($html->find(".employee_list thead tbody tr") as $element)		
+	{
+			echo $name = $element->find("//*[@id='employee_list']/tbody[1]/tr/td[1]")->plaintext;
+			'<br/>';
+	}	
 ?>
+
