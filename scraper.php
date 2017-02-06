28e0c65b1b0cb6db62464d606b120b075238716b
vikash12345 <vikash@backgroundcheck.email>
2017-02-06 12:00:20 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,12 +1,51 @@
-<?php 
-	require 'simple_html_dom.php';
-	$MyWebsite = 'http://www.peshawarhighcourt.gov.pk/PHCCMS/reportedJudgments.php?action=search';
-	$html = file_get_html($MyWebsite);
-	echo $html;
-	foreach($html->find(".employee_list thead tbody tr") as $element)		
-	{
-			echo $name = $element->find("//*[@id='employee_list']/tbody[1]/tr/td[1]");
-			'<br/>';
-	}	
-?>
 
+
+	<?php
+	
+	 require 'simple_html_dom.php';
+
+ $cHeadres = array(
+      'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language: en-US,en;q=0.5',
+      'Connection: Keep-Alive',
+      'Pragma: no-cache',
+      'Cache-Control: no-cache'
+     );
+
+ $MyWebsite = 'http://www.peshawarhighcourt.gov.pk/PHCCMS/reportedJudgments.php?action=search';
+
+ function dlPage($href) {
+  global $cHeadres;
+
+  $ch = curl_init();
+  if($ch){
+   curl_setopt($ch, CURLOPT_URL, $href);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $cHeadres);
+   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt');
+   curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt');
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+   curl_setopt($ch, CURLOPT_HEADER, false);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)");
+   $str = curl_exec($ch);
+   curl_close($ch);
+
+   $dom = new simple_html_dom();
+   $dom->load($str);
+   return $dom;
+  }
+ }
+
+ $maincode = dlPage($MyWebsite);
+ 
+ 
+ 
+
+ foreach($maincode->find("//*[@id='employee_list']/tbody[1]/tr") as $element) {
+  echo $name = $element->find("td", 0)->plaintext;
+  echo '<br/>';
+ } 
+ 
+ ?>
